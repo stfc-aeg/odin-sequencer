7cac76e1bc481a50bf50b9bac6d9b312e58827a7
Viktor Bozhinov <viktor.bozhinov@stfc.ac.uk>
2020-09-14 09:12:19 +0100

    #2 Remove debug printing and tidy code

--- tests/testutils.py
+++ tests/testutils.py
@@ -9,7 +9,11 @@ import os
 
 
 def modify_test_reload_module_file(shared_datadir):
-    """This method modifies the content of the test_reload.py module"""
+    """
+    This method modifies the content of the test_reload.py module.
+    There is a small delay to ensure that the modification does not
+    happen at the same time as the creation of the module.
+    """
     time.sleep(0.1)
     module = shared_datadir.joinpath('test_reload.py')
 
@@ -19,7 +23,11 @@ def get_message():
 
 
 def modify_with_dependency_module_file(shared_datadir):
-    """This method modifies the content of the with_dependency.py module"""
+    """
+    This method modifies the content of the with_dependency.py module.
+    There is a small delay to ensure that the modification does not
+    happen at the same time as the creation of the module.
+    """
     time.sleep(0.1)
     module = shared_datadir.joinpath('with_dependency.py')
 
@@ -36,13 +44,11 @@ def await_queue_size(file_watcher, expected_queue_size):
     size number. The loop exists if the number is not reached after 10 seconds.
     """
 
-    for i in range(60, 0, -1):
+    for _ in range(30, 0, -1):
         actual_queue_size = file_watcher.modified_files_queue.qsize()
-        print("i = {}, expected_queue_size = {}, actual_queue_size = {}".format(
-            i, str(expected_queue_size), str(actual_queue_size)))
-
         if actual_queue_size == expected_queue_size:
             break
+
         time.sleep(0.5)
 
 
@@ -61,13 +67,11 @@ def was_file_modified(path, last_modified_time):
     :return: return False if modification did not occur after 15 seconds,
                 otherwise return True
     """
-    for i in range(60, 0, -1):
+    for _ in range(30, 0, -1):
         modified_time = get_last_modified_file_time(path)
-        print("i = {}, last_modified_time = {}, modified_time = {}".format(
-            i, str(last_modified_time), str(modified_time)))
-
         if modified_time != last_modified_time:
             return True
+
         time.sleep(0.5)
 
     return False
