a9019a28c8bc3ab583712634efc5bc0c5ab62efa
Viktor Bozhinov <viktor.bozhinov@stfc.ac.uk>
2020-11-03 14:22:17 +0000

    #8 Remove unnecessary implementation in CommandSequencer

--- src/odin_sequencer/command_sequencer.py
+++ src/odin_sequencer/command_sequencer.py
@@ -1,41 +1,48 @@
+"""CommandSequencer module
+
+Module which facilities communication to the command sequencer manager.
+"""
+
 from odin_sequencer import CommandSequenceManager, CommandSequenceError
 
 from odin.adapters.parameter_tree import ParameterTree, ParameterTreeError
 
 
 class CommandSequencer:
+    """CommandSequencer object representing the command sequencer manager.
+
+    Facilities communcation to the command sequence manager."""
 
     def __init__(self, path_or_paths=None):
+        """Initialise the CommandSequencer object.
 
+        This constructor initialises the CommandSequencer object, creating a command
+        sequencer manager, and building a parameter tree for it which allows clients
+        to communicate with the manager.
+        """
         self.manager = CommandSequenceManager(path_or_paths)
-        self.auto_reload = False
-
-        self.param_tree = ParameterTree({
-            'sequences': {
-                sequences_name: ({parameter.name: parameter.default for parameter in list(
-                    self.manager.sequence_signatures[sequences_name].parameters.values())}) for sequences_name in self.manager.sequence_signatures
-            },
-
-            'auto_reload': (lambda: self.auto_reload, self.set_auto_reload)
-        })
+        self.param_tree = ParameterTree({})
 
     def get(self, path):
+        """Get parameters from the underlying parameter tree.
+
+        This method returns the parameter tree for use by clients via CommandSequenceManagerAdapter.
+
+        :param path: path of parameter tree to get
+        :returns: parameter tree at that path as a dictionary
+        """
         return self.param_tree.get(path)
 
     def set(self, path, data):
+        """Set parameter in the parameter tree.
+
+        This method simply wraps underlying ParameterTree method so that an exceptions can be
+        re-raised with an appropriate CommandSequenceError.
+
+        :param path: path of parameter tree to set values for
+        :param data: dictionary of new data values to set in the parameter tree
+        """
         try:
             self.param_tree.set(path, data)
-        except ParameterTreeError as e:
-            raise CommandSequenceError(e)
-
-    def set_auto_reload(self, auto_reload):
-
-        auto_reload = bool(auto_reload)
-
-        if auto_reload != self.auto_reload:
-            if auto_reload:
-                self.manager.enable_auto_reload()
-            else:
-                self.manager.disable_auto_reload()
-
-            self.auto_reload = auto_reload
+        except ParameterTreeError as error:
+            raise CommandSequenceError(error)
