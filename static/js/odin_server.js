465191d125b015e5283c62df3ff2352c7b9dfa51
Viktor Bozhinov <viktor.bozhinov@stfc.ac.uk>
2020-11-06 10:54:09 +0000

    #20 Handle API GET and PUT requests

---
+++ static/js/odin_server.js
@@ -0,0 +1,22 @@
+/**
+ * This function handles the GET requests to the backend's API. 
+ */
+function apiGET(path) {
+    return $.ajax({
+        url: `api/${api_version}/${adapter}/${path}`,
+        type: 'GET',
+        dataType: 'json'
+    });
+}
+
+/**
+ * This function handles the API requests to the backend's API.
+ */
+function apiPUT(data, path='') {
+    return $.ajax({
+        url: `api/${api_version}/${adapter}/${path}`,
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(data)
+    });
+}
