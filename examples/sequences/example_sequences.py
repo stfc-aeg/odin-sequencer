fd7fb1c3d37f4834ec2598386847837615b9bce6
rhysrevans3 <rhys.r.evans3@gmail.com>
2021-01-29 08:34:00 +0000

    Updating example sequences.

--- examples/sequences/example_sequences.py
+++ examples/sequences/example_sequences.py
@@ -1,26 +1,23 @@
-requires = ['spi_commands', 'qem_plotcoarseMulti']
+requires = ['spi_commands']
 provides = ['test_sequence', 'another_sequence']
 
 def test_sequence(a_val=123, b='hello'):
 
     print("Running test sequence")
 
-    dev = get_context('test_device')
     spi_read(8) #part of spi_commands.py
     spi_write([0x34]) # part of spi_commands.py
     #load_dacs()
     #calibrate()
 
-    queue = get_context('process_writer')
-    queue.run('add', 4, 3)
-    queue.group('add', range(10), 3)
+    # Example loading a context
+    dev = get_context('test_device')
+    reg_val = dev.read_reg()
+    print("Read register value", reg_val)
 
     for i in range(10):
         spi_write([i])
     
-    reg_val = dev.read_reg()
-    print("Read register value", reg_val)
-
     reg_val += 1
     dev.write_reg(0x33, reg_val)
 
